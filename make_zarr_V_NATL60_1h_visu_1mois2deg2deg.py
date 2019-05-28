1e8b9f927bc4be34055d4414d3f0fec34afc83f6
albert7a <albert7a@login1.occigen>
2019-05-28 14:53:36 +0200

    up

--- make_zarr_V_NATL60_1h_visu_1mois2deg2deg.py
+++ make_zarr_V_NATL60_1h_visu_1mois2deg2deg.py
@@ -14,39 +14,49 @@ compressor = zarr.Blosc(cname='zstd', clevel=3, shuffle=2)
 ds=xr.open_mfdataset('/store/albert7a/NATL60/NATL60-CJM165-S/1h/surf-U-V/NATL60-CJM165_y2012m10d??.1h_SSV.nc', parallel=True, concat_dim='time_counter',chunks={'time_counter':672,'y':120,'x':120})
 
 encoding = {vname: {'compressor': compressor} for vname in ds.variables}                                  
-ds.to_zarr(store='/store/albert7a/NATL60/NATL60-CJM165-S/1h/surf-U-V/zarr/NATL60-CJM165-SSV-1h-1m2deg2deg', encoding=encoding)
-zrtot=zarr.open('/store/albert7a/NATL60/NATL60-CJM165-S/1h/surf-U-V/zarr/NATL60-CJM165-SSV-1h-1m2deg2deg',mode='a')
+ds.to_zarr(store='/scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSV/zarr/NATL60-CJM165-SSV-1h-1m2deg2deg', encoding=encoding)
+zrtot=zarr.open('/scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSV/zarr/NATL60-CJM165-SSV-1h-1m2deg2deg',mode='a')
 
-for m in np.arange(12,13):
+for m in np.arange(11,13):
         print('beginning month ',str(m))
 	ds=xr.open_mfdataset('/store/albert7a/NATL60/NATL60-CJM165-S/1h/surf-U-V/NATL60-CJM165_y2012m'+str(m)+'d??.1h_SSV.nc', parallel=True, concat_dim='time_counter',chunks={'time_counter':672,'y':120,'x':120})
-	ds.to_zarr(store='/store/albert7a/NATL60/NATL60-CJM165-S/1h/surf-U-V/zarr/zarr_SSV_2012m'+str(m)+'-1m2deg2deg', encoding=encoding)
-	zr=zarr.open('/store/albert7a/NATL60/NATL60-CJM165-S/1h/surf-U-V/zarr/zarr_SSV_2012m'+str(m)+'-1m2deg2deg',mode='r')
+	encoding = {vname: {'compressor': compressor} for vname in ds.variables}                                  
+	ds.to_zarr(store='/scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSV/zarr/zarr_SSV_2012m'+str(m)+'-1m2deg2deg', encoding=encoding)
+        print('ending month ',str(m))
+
+for m in np.arange(1,10):
+        print('beginning month ',str(m))
+        ds=xr.open_mfdataset('/store/albert7a/NATL60/NATL60-CJM165-S/1h/surf-U-V/NATL60-CJM165_y2013m0'+str(m)+'d??.1h_SSV.nc', parallel=True, concat_dim='time_counter',chunks={'time_counter':672,'y':120,'x':120})
+	encoding = {vname: {'compressor': compressor} for vname in ds.variables}                                  
+        ds.to_zarr(store='/scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSV/zarr/zarr_SSV_2013m0'+str(m)+'-1m2deg2deg', encoding=encoding)
+        print('ending month ',str(m))
+
+for m in np.arange(11,13):
+        print('beginning month ',str(m))
+	zr=zarr.open('/scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSV/zarr/zarr_SSV_2012m'+str(m)+'-1m2deg2deg',mode='r')
 	for key in [k for k in zr.array_keys() if k not in ['nav_lat','nav_lon']]:
 	        zrtot[key].append(zr[key])
         print('ending month ',str(m))
 
 for m in np.arange(1,10):
         print('beginning month ',str(m))
-        ds=xr.open_mfdataset('/store/albert7a/NATL60/NATL60-CJM165-S/1h/surf-U-V/NATL60-CJM165_y2013m0'+str(m)+'d??.1h_SSV.nc', parallel=True, concat_dim='time_counter',chunks={'time_counter':672,'y':120,'x':120})
-        ds.to_zarr(store='/store/albert7a/NATL60/NATL60-CJM165-S/1h/surf-U-V/zarr/zarr_SSV_2013m0'+str(m)+'-1m2deg2deg', encoding=encoding)
-        zr=zarr.open('/store/albert7a/NATL60/NATL60-CJM165-S/1h/surf-U-V/zarr/zarr_SSV_2013m0'+str(m)+'-1m2deg2deg',mode='r')
+        zr=zarr.open('/scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSV/zarr/zarr_SSV_2013m0'+str(m)+'-1m2deg2deg',mode='r')
         for key in [k for k in zr.array_keys() if k not in ['nav_lat','nav_lon']]:
                 zrtot[key].append(zr[key])
         print('ending month ',str(m))
 
-name='/store/albert7a/NATL60/NATL60-CJM165-S/1h/surf-U-V/zarr/NATL60-CJM165-SSV-1h-1m2deg2deg'
-variable='time_counter'
-for dname in [f for f in os.listdir(name) if not f.startswith('.')]:
-     for fname in [f for f in os.listdir(f'{name}/{variable}') if not f.startswith('.')]:
-         os.remove(f'{name}/{variable}/{fname}')
+name='/scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSV/zarr/NATL60-CJM165-SSV-1h-1m2deg2deg'
+for variable in 'time_counter' 'nav_lat' 'nav_lon':
+	for dname in [f for f in os.listdir(name) if not f.startswith('.')]:
+	     for fname in [f for f in os.listdir(f'{name}/{variable}') if not f.startswith('.')]:
+	         os.remove(f'{name}/{variable}/{fname}')
 
 #ds=xr.open_mfdataset('/store/albert7a/NATL60/NATL60-CJM165-S/1h/SSH/NATL60-CJM165_y2012m10d01-y2013m09d30.1h_time.nc', parallel=True, concat_dim='time_counter')
 #encoding = {vname: {'compressor': compressor} for vname in ds.variables}
 #ds.to_zarr(store='/scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSH/zarr_time', encoding=encoding)
 
-!cp -r /scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSH/zarr_time/time_counter /store/albert7a/NATL60/NATL60-CJM165-S/1h/surf-U-V/zarr/NATL60-CJM165-SSV-1h-1m2deg2deg/
+!cp -r /scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSH/zarr_time/time_counter /scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSV/zarr/NATL60-CJM165-SSV-1h-1m2deg2deg/
 
 
-ds_z = xr.open_zarr('/store/albert7a/NATL60/NATL60-CJM165-S/1h/surf-U-V/zarr/NATL60-CJM165-SSV-1h-1m2deg2deg')
+ds_z = xr.open_zarr('/scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSV/zarr/NATL60-CJM165-SSV-1h-1m2deg2deg')
 ds_z
---
+++ make_zarr_V_NATL60_1h_visu_2D.py
@@ -0,0 +1,54 @@
+import xarray as xr
+import dask
+import dask.threaded
+import dask.multiprocessing
+from dask.distributed import Client
+import zarr     
+import numpy as np
+import os
+ 
+
+compressor = zarr.Blosc(cname='zstd', clevel=3, shuffle=2)                                                
+
+
+ds=xr.open_mfdataset('/store/albert7a/NATL60/NATL60-CJM165-S/1h/surf-U-V/NATL60-CJM165_y2012m10d??.1h_SSV.nc', parallel=True, concat_dim='time_counter',chunks={'time_counter':1})
+
+encoding = {vname: {'compressor': compressor} for vname in ds.variables}                                  
+ds.to_zarr(store='/scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSV/zarr/NATL60-CJM165-SSV-1h-2D', encoding=encoding)
+zrtot=zarr.open('/scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSV/zarr/NATL60-CJM165-SSV-1h-2D',mode='a')
+
+for m in np.arange(11,13):
+	print('beginning month ',str(m))
+#	ds=xr.open_mfdataset('/store/albert7a/NATL60/NATL60-CJM165-S/1h/surf-U-V/NATL60-CJM165_y2012m'+str(m)+'d??.1h_SSV.nc', parallel=True, concat_dim='time_counter',chunks={'time_counter':1})
+#	encoding = {vname: {'compressor': compressor} for vname in ds.variables}                                  
+#	ds.to_zarr(store='/scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSV/zarr/zarr_SSV_2012m'+str(m)+'-2D', encoding=encoding)
+	zr=zarr.open('/scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSV/zarr/zarr_SSV_2012m'+str(m)+'-2D',mode='r')
+	for key in [k for k in zr.array_keys() if k not in ['nav_lat','nav_lon']]:
+	        zrtot[key].append(zr[key])
+	print('ending month ',str(m))
+
+for m in np.arange(1,10):
+	print('beginning month ',str(m))
+#	ds=xr.open_mfdataset('/store/albert7a/NATL60/NATL60-CJM165-S/1h/surf-U-V/NATL60-CJM165_y2013m0'+str(m)+'d??.1h_SSV.nc', parallel=True, concat_dim='time_counter',chunks={'time_counter':1})
+#	encoding = {vname: {'compressor': compressor} for vname in ds.variables}                                  
+#	ds.to_zarr(store='/scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSV/zarr/zarr_SSV_2013m0'+str(m)+'-2D', encoding=encoding)
+	zr=zarr.open('/scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSV/zarr/zarr_SSV_2013m0'+str(m)+'-2D',mode='r')
+	for key in [k for k in zr.array_keys() if k not in ['nav_lat','nav_lon']]:
+		zrtot[key].append(zr[key])
+	print('ending month ',str(m))
+
+name='/scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSV/zarr/NATL60-CJM165-SSV-1h-2D'
+variable='time_counter'
+for dname in [f for f in os.listdir(name) if not f.startswith('.')]:
+     for fname in [f for f in os.listdir(f'{name}/{variable}') if not f.startswith('.')]:
+         os.remove(f'{name}/{variable}/{fname}')
+
+ds=xr.open_mfdataset('/store/albert7a/NATL60/NATL60-CJM165-S/1h/SSH/NATL60-CJM165_y2012m10d01-y2013m09d30.1h_time.nc', parallel=True, concat_dim='time_counter')
+encoding = {vname: {'compressor': compressor} for vname in ds.variables}
+ds.to_zarr(store='/scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSH/zarr_time', encoding=encoding)
+
+!cp -r /scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSH/zarr_time/time_counter /scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSV/zarr/NATL60-CJM165-SSV-1h-2D/
+
+
+ds_z = xr.open_zarr('/scratch/cnt0024/hmg2840/albert7a/NATL60/NATL60-CJM165-S/1h/SSV/zarr/NATL60-CJM165-SSV-1h-2D')
+ds_z
